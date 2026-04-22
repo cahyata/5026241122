9c5322973a46f1519fe19d1bda0d89ae448e67f3
jerji <guardbros06@gmail.com>
2026-04-22 12:00:46 +0700

    pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController ;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('halo', function () {
-	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <u>www.malasngoding.com</u>";
 });
 
 Route::get('blog', function () {
 	return view('blog');
 });
-
-Route::get('blog', function () {
-	return view('blog');
+Route::get('pert5', function () {
+	return view('pertemuan5');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('biodata', [DosenController::class, 'biodata']);
