c89f1117c2b393cd4706ff5712e97ff26683eebf
jerji <guardbros06@gmail.com>
2026-04-22 11:08:25 +0700

    pertemuan 7

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "Halo, Selamat datang di tutorial laravel www.malasngoding.com";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
