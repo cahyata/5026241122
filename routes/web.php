74637f12c1368b4f95b84983001ad69801524caa
jerji <guardbros06@gmail.com>
2026-05-20 12:14:15 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\PegawaiDBController;
 use App\Http\Controllers\BlogController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +13,10 @@ Route::get('/', function () {
 
 Route::get('halo', function () {
 	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <u>www.malasngoding.com</u>";
+});
+
+Route::get('index', function () {
+	return view('index');
 });
 
 Route::get('blog', function () {
@@ -48,10 +54,15 @@ Route::get('menu', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
 //form
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawailama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+
+
+//route CRUD
+Route::get('/pegawai',[PegawaiDBController::class, 'index2']);
