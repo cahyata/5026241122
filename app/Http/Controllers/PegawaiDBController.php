2720c87ccf2cb1777839b1599fb98f769c24a6ac
jerji <guardbros06@gmail.com>
2026-06-08 17:36:40 +0700

    pertemuan 14

--- app/Http/Controllers/PegawaiDBController.php
+++ app/Http/Controllers/PegawaiDBController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 
+
 class PegawaiDBController extends Controller
 {
+
     public function index2()
     {
     	// mengambil data dari table pegawai
